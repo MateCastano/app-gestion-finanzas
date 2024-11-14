a978ddf02e64e2c8f316c51c53ca8a4baba2e6cf
MateCastano <mateonc@gmail.com>
2024-11-14 18:30:55 -0300

    login dto, controller and route

--- src/routes/user-router.js
+++ src/routes/user-router.js
@@ -1,10 +1,12 @@
+import userLoginController from "#Controllers/user-controllers/user-login-controller.js";
 import userRegisterController from "#Controllers/user-controllers/user-register-controller.js";
+import userLoginDTO from "#Dto/dto-user/user-login-dto.js";
 import userRegisterDTO from "#Dto/dto-user/user-register-dto.js";
 import { Router } from "express";
 
 const userRouter = Router();
 
-
 userRouter.post('/register', userRegisterDTO, userRegisterController);
+userRouter.post('/login', userLoginDTO, userLoginController);
 
 export default userRouter;
